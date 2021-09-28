1f58537c71dce1109ddfb1f6d0f7d129d6ebf849
lopnesh <lopnesh@gmail.com>
2021-09-28 22:45:57 +0300

    add egress

--- instance.tf
+++ instance.tf
@@ -18,11 +18,17 @@ resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
   vpc_id = "vpc-321a8358"
   description = "Allow ssh connections on port 22"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
